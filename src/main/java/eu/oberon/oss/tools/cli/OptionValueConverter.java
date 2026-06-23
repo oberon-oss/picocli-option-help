64aee4ee4feaedb85d1624cae0b2454cf6dae0f3
TigerLilly <info@oberon-oss.eu>
2026-06-23 12:30:47 +0200

    Enhance `OptionValueConverter` JavaDocs with proper formatting and additional details

--- src/main/java/eu/oberon/oss/tools/cli/OptionValueConverter.java
+++ src/main/java/eu/oberon/oss/tools/cli/OptionValueConverter.java
@@ -12,7 +12,8 @@ import java.util.stream.Collectors;
  * <p>
  * For example, assuming there is a class '<b>Action</b>' representing a limited set of allowed values, then the converter for the contained values in
  * '<b>Action</b>' can be registered as follows:
- * <pre>
+ *
+ * <pre>{@code
  * {
  * ...
  * CommandLine commandLine = new CommandLine(new SomeCommand());
@@ -25,11 +26,23 @@ import java.util.stream.Collectors;
  * commandLine.execute(args);
  * ...
  * }
- * </pre>
+ * }</pre>
+ *
+ * @param <T> the type of the option value
+ *
+ * @author TigerLilly64
+ * @since 1.1.0
  */
 public final class OptionValueConverter<T> implements ITypeConverter<T> {
     private final Map<String, T> valuesByName;
 
+    /**
+     * Creates a new {@link OptionValueConverter} object.
+     *
+     * @param valuesProvider The option value provider that handles the option values to be converted.
+     *
+     * @since 1.1.0
+     */
     public OptionValueConverter(OptionValuesProvider<T> valuesProvider) {
         this.valuesByName = valuesProvider.values().stream()
                 .collect(Collectors.toMap(
